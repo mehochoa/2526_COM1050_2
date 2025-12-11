df47964b24c440407773004a4ae544f6d8a96d1e
vhtd <25020063@vnu.edu.vn>
2025-12-11 10:52:26 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,5 @@
+n=int(input("Nhap vao so nguyen duong n: "))
+if n%2==0:
+    print('So cach phan tich n thanh tong hai so khong am: ',n/2+1)
+if n%2!=0:
+    print('So cach phan tich n thanh tong hai so khong am: ',n//2+1)
---
+++ 4.py
@@ -0,0 +1,4 @@
+n=int(input("Nhap vao so nguyen duong n: "))
+if n%3!=0:
+    print("Co 0 cach phan tich")
+print("Co 1 cach phan tich") 
